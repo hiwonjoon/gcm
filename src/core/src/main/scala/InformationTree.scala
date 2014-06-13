ad8316d38e7bb7b66086545a31173f946fd911fc
Goo WonJoon <hi.wonjoon@gmail.com>
2014-06-13 21:19:56 +0900

    여러 종류의 매크로 한번에 디텍션 하기

--- src/core/src/main/scala/InformationTree.scala
+++ src/core/src/main/scala/InformationTree.scala
@@ -10,8 +10,8 @@ import scala.compat.Platform
 
 case class GetPerformance(sendTo:ActorRef)
 case class DeleteUser()
-case class GetVector(id:String,sendTo:ActorRef)
-case class Vectors(id:String,vec:Seq[Int])
+case class GetVector(kind:Int,id:String,sendTo:ActorRef)
+case class Vectors(id:String,vec:(Seq[Int]))
 case class VectorsEmpty(id:String)
 case class AllVectorSent(backend:ActorRef)
 case class VectorList(backend:ActorRef, map : HashMap[String,Seq[Int]])
@@ -19,11 +19,11 @@ case class VectorList(backend:ActorRef, map : HashMap[String,Seq[Int]])
 class InformationTree(parent : ActorRef) extends Actor {
   var user_vector_map = new HashMap[String,ActorRef];
   def receive = {
-    case GetVector(_,sendTo) => {
+    case GetVector(kind,_,sendTo) => {
       val oneTreeGatherer = context.system.actorOf(Props(new OneTreeGatherer(user_vector_map.size,parent,sendTo)));
       user_vector_map.foreach{
         case(id,ref) => {
-          ref ! GetVector(id,oneTreeGatherer)
+          ref ! GetVector(kind,id,oneTreeGatherer)
         }
       }
     }
@@ -55,7 +55,7 @@ class UserVector(id:String) extends Actor {
   var last_vector_calculated : Long = 0;
   var list : scala.collection.immutable.List[Job] = Nil;
   def receive = {
-    case GetVector(_,sendTo) => {
+    case GetVector(kind,_,sendTo) => {
         if( last_inserted <= Platform.currentTime - maximum )
         {
           sender ! DeleteUser()
@@ -65,39 +65,42 @@ class UserVector(id:String) extends Actor {
           sendTo ! VectorsEmpty(id)
         }
         else {
-        val moveList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserMove])
+          val moveList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserMove])
 
-        val moveEast = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 0)
-        val moveWest = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 1)
-        val moveSouth = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 2)
-        val moveNorth = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 3)
+          val moveEast = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 0)
+          val moveWest = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 1)
+          val moveSouth = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 2)
+          val moveNorth = moveList.count(job => job.asInstanceOf[UserMove].getDir() == 3)
 
-        val battleList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserBattleResult])
+          val battleList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserBattleResult])
 
-        val pveResult = battleList.count(job => job.asInstanceOf[UserBattleResult].winner.id == id && job.asInstanceOf[UserBattleResult].winner.isNpc == false && job.asInstanceOf[UserBattleResult].loser.isNpc == true)
+          val pveResult = battleList.count(job => job.asInstanceOf[UserBattleResult].winner.id == id && job.asInstanceOf[UserBattleResult].winner.isNpc == false && job.asInstanceOf[UserBattleResult].loser.isNpc == true)
 
-        val pvpList = battleList.filter(job => job.asInstanceOf[UserBattleResult].winner.id == id && job.asInstanceOf[UserBattleResult].winner.isNpc == false && job.asInstanceOf[UserBattleResult].loser.isNpc == false)
-        var pvpResult:Seq[Int] = Nil
-        for(i <- 0 to 15)
-        {
-          pvpResult :+= pvpList.count(job => job.asInstanceOf[UserBattleResult].GetZone() == i)
-        }
+          val pvpList = battleList.filter(job => job.asInstanceOf[UserBattleResult].winner.id == id && job.asInstanceOf[UserBattleResult].winner.isNpc == false && job.asInstanceOf[UserBattleResult].loser.isNpc == false)
+          var pvpResult:Seq[Int] = Nil
+          for(i <- 0 to 15)
+          {
+            pvpResult :+= pvpList.count(job => job.asInstanceOf[UserBattleResult].GetZone() == i)
+          }
 
 
-        var skillList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserSkill])
+          var skillList = list.filter(job => job.getTime() >= last_inserted - check_interval && job.isInstanceOf[UserSkill])
 
-        var skillA = skillList.count(job => job.asInstanceOf[UserSkill].skill.equals("A"))
-        var skillB = skillList.count(job => job.asInstanceOf[UserSkill].skill.equals("B"))
+          var skillA = skillList.count(job => job.asInstanceOf[UserSkill].skill.equals("A"))
+          var skillB = skillList.count(job => job.asInstanceOf[UserSkill].skill.equals("B"))
 
-        // 반복적인 움직임 패턴 감지
-//        sendTo ! Vectors(id, 0::moveEast::moveWest::moveSouth::moveNorth::Nil )
+          kind match {
+            case 0 =>
+              // 반복적인 움직임 패턴 감지
+              sendTo ! Vectors(id, 0::moveEast::moveWest::moveSouth::moveNorth::Nil )
+            case 1 =>
+              // 반복적인 사냥 감지
+              sendTo ! Vectors(id, 1::pveResult::skillA::skillB::Nil )
+            case 2 =>
+              //특정 지역에서 유저를 계속 죽이고 다니는 놈 감지
+              sendTo ! Vectors(id, 2+:pvpResult)
 
-        // 반복적인 사냥 감지
-       sendTo ! Vectors(id, 1::pveResult::skillA::skillB::Nil )
-
-        // 특정 지역에서 유저를 계속 죽이고 다니는 놈 감지
-//      sendTo ! Vectors(id, 2+:pvpResult)
-
+         }
         last_vector_calculated = Platform.currentTime
       }
     }
